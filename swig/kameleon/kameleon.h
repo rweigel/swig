dade59e37118a05c6971d672839f098c658d56be
weigel <rweigel@gmu.edu>
2020-07-06 21:32:51 -0400

    mv kameleon to rweigel/kameleonV repository

--- swig/kameleon/kameleon.h
+++
@@ -1,7 +0,0 @@
-#ifndef _kameleon
-#define _kameleon
-
-#include <vector>
-std::vector<float> runner (char* filename, std::vector<float> x, std::vector<float> y, std::vector<float> z, int N);
-
-#endif
